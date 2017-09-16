50cc35ef0998c2155e40d95f84d0f22c0fd18bc3
Meir Rosilio <meirosilio@gmail.com>
2017-09-16 22:33:31 +0300

    working with bootstarp + rails + static pages

---
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+
+  def index
+    @app_name='Sample App'
+  end
+
+  def help
+  end
+
+  def about
+
+  end
+
+  def contact
+  end
+
+  def news
+
+  end
+
+
+end
---
+++ sample_app/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#index'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/news', to:'static_pages#news'
+
+  get  '/signup',  to: 'users#new'
+
+end
